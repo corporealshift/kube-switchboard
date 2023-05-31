c137957dce72050d1c396125bec63c600258726f
Kyle Diedrick <corporealshift@gmail.com>
2023-05-31 11:10:35 -0400

    Add tests for the bad pod function

--- src/kube_res/pods.rs
+++ src/kube_res/pods.rs
@@ -10,9 +10,9 @@ pub fn check_pods(namespace: String, tx: Sender<KubeMessage>) {
     tokio::spawn(async move {
         match Client::try_default().await {
             Ok(client) => {
-                let pods: Api<Pod> = Api::namespaced(client, namespace.as_str());
-                let all_pods = pods.list(&ListParams::default()).await;
-                let bad_pods = all_pods.map(|list| {
+                let pods_request: Api<Pod> = Api::namespaced(client, namespace.as_str());
+                let all_pods = pods_request.list(&ListParams::default()).await;
+                let pods = all_pods.map(|list| {
                     let all_pods_vec: Vec<Pod> = list.iter().map(|p| p.clone()).collect();
                     println!("got pods list! ns: {}", namespace);
                     (
@@ -20,7 +20,7 @@ pub fn check_pods(namespace: String, tx: Sender<KubeMessage>) {
                         only_bad_pods(&list).collect::<Vec<Option<Pod>>>(),
                     )
                 });
-                match bad_pods {
+                match pods {
                     Ok((all_pods, bad_pods)) => {
                         if bad_pods.iter().count() > 0 {
                             let _ = tx
@@ -81,6 +81,53 @@ fn error(err: Error) -> KubeMessage {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use k8s_openapi::api::core::v1::{Pod, PodStatus};
+    use kube::api::ObjectList;
+
     #[test]
-    fn good_pods_are_ignored() {}
+    pub fn good_pods_are_ignored() {
+        let pods = ObjectList {
+            metadata: Default::default(),
+            items: vec![
+                pod(Some("Running".to_owned())),
+                pod(Some("Succeeded".to_owned())),
+                pod(Some("Pending".to_owned())),
+                pod(Some("Failed".to_owned())),
+            ],
+        };
+
+        assert_eq!(only_bad_pods(&pods).count(), 2);
+    }
+
+    #[test]
+    fn pods_with_no_status_are_bad() {
+        let pods = ObjectList {
+            metadata: Default::default(),
+            items: vec![pod(Some("Running".to_owned())), pod(None)],
+        };
+        assert_eq!(only_bad_pods(&pods).count(), 1);
+    }
+
+    fn pod(phase: Option<String>) -> Pod {
+        Pod {
+            metadata: Default::default(),
+            spec: None,
+            status: Some(PodStatus {
+                conditions: None,
+                container_statuses: None,
+                ephemeral_container_statuses: None,
+                host_ip: None,
+                init_container_statuses: None,
+                message: None,
+                nominated_node_name: None,
+                phase,
+                pod_ip: None,
+                pod_ips: None,
+                qos_class: None,
+                reason: None,
+                start_time: None,
+            }),
+        }
+    }
 }
